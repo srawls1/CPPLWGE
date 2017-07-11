6d17e3d1a52a57384b555abc9e8e24f75f130eee
Spencer Rawls <spencerrawls@tamu.edu>
2017-07-10 22:37:07 -0500

    2D analogues for al 3D vector operations

---
+++ CPPLWGE/CPPLWGE/Vector2D.cpp
@@ -0,0 +1,200 @@
+#include "Vector2D.h"
+
+
+Vector2D::Vector2D(f32 xx = 0.f, f32 yy = 0.f)
+	: x(xx), y(yy)
+{}
+
+void Vector2D::normalize()
+{
+	f32 mag = magnitude();
+	x /= mag;
+	y /= mag;
+}
+
+Vector2D Vector2D::unitVector()
+{
+	return *this / magnitude();
+}
+
+f32 Vector2D::magnitude()
+{
+	return Math::sqrt(magnitudeSquared());
+}
+
+f32 Vector2D::magnitudeSquared()
+{
+	return x * x + y * y;
+}
+
+f32 Vector2D::projectOntoMagnitude(Vector2D other)
+{
+	return Vector2D::dotProduct(*this, other) / other.magnitude();
+}
+
+Vector2D Vector2D::projectOnto(Vector2D other)
+{
+	return projectOntoMagnitude(other) * other.unitVector();
+}
+
+Vector2D Vector2D::orthogonalProjectionOnto(Vector2D other)
+{
+	return *this - projectOnto(other);
+}
+
+Vector2D Vector2D::reflectedAcross(Vector2D normal)
+{
+	return 2 * projectOnto(normal) - *this;
+}
+
+void Vector2D::operator+=(Vector2D v)
+{
+	x += v.x;
+	y += v.y;
+}
+
+void Vector2D::operator-=(Vector2D v)
+{
+	x -= v.x;
+	y -= v.y;
+}
+
+void Vector2D::operator*=(f32 scalar)
+{
+	x *= scalar;
+	y *= scalar;
+}
+
+void Vector2D::operator/=(f32 scalar)
+{
+	x /= scalar;
+	y /= scalar;
+}
+
+Vector2D Vector2D::vectorAdd(Vector2D v1, Vector2D v2)
+{
+	return Vector2D(v1.x + v2.x, v1.y + v2.y);
+}
+
+Vector2D Vector2D::vectorSubtract(Vector2D v1, Vector2D v2)
+{
+	return Vector2D(v1.x - v2.x, v1.y - v2.y);
+}
+
+Vector2D Vector2D::scalarMultiply(f32 scalar, Vector2D v)
+{
+	return Vector2D(scalar * v.x, scalar * v.y);
+}
+
+f32 Vector2D::dotProduct(Vector2D v1, Vector2D v2)
+{
+	return v1.x * v2.x + v1.y * v2.y;
+}
+
+Vector2D Vector2D::scale(Vector2D v1, Vector2D v2)
+{
+	return Vector2D(v1.x * v2.x, v1.y * v2.y);
+}
+
+f32 Vector2D::crossProduct(Vector2D v1, Vector2D v2)
+{
+	return v1.x * v2.y - v1.y * v2.x;
+}
+
+f32 Vector2D::distance(Vector2D v1, Vector2D v2)
+{
+	return vectorSubtract(v1, v2).magnitude();
+}
+
+f32 Vector2D::distanceSquared(Vector2D v1, Vector2D v2)
+{
+	return vectorSubtract(v1, v2).magnitudeSquared();
+}
+
+f32 Vector2D::cosAngleBetween(Vector2D v1, Vector2D v2)
+{
+	return dotProduct(v1, v2) / (v1.magnitude() * v2.magnitude());
+}
+
+f32 Vector2D::angleBetween(Vector2D v1, Vector2D v2)
+{
+	return Math::arccos(cosAngleBetween(v1, v2));
+}
+
+Vector2D Vector2D::lerp(Vector2D v1, Vector2D v2, f32 alpha)
+{
+	return (1 - alpha) * v1 + alpha * v2;
+}
+
+Vector2D Vector2D::slerp(Vector2D v1, Vector2D v2, f32 alpha)
+{
+	f32 angle = angleBetween(v1, v2);
+	f32 sina = Math::sin(angle);
+	return Math::sin((1 - alpha) * angle) / sina * v1 + Math::sin(alpha * angle) / sina * v2;
+}
+
+Vector2D Vector2D::moveTowards(Vector2D from, Vector2D to, f32 maxStep)
+{
+	Vector2D dif = to - from;
+	if (dif.magnitude() < maxStep)
+	{
+		return to;
+	}
+	return from + dif.unitVector() * maxStep;
+}
+
+Vector2D Vector2D::rotateTowards(Vector2D from, Vector2D to, f32 maxRotateStep, f32 maxMagnitudeStep)
+{
+	f32 fromMagnitude = from.magnitude();
+	f32 toMagnitude = to.magnitude();
+	f32 magDif = toMagnitude - fromMagnitude;
+	f32 retMag = Math::abs(magDif) < maxMagnitudeStep ? fromMagnitude + maxMagnitudeStep : toMagnitude;
+
+	from.normalize();
+	to.normalize();
+	f32 angle = dotProduct(from, to);
+	f32 alpha = Math::min(1.f, angle / maxRotateStep);
+	return retMag * slerp(from, to, alpha);
+}
+
+Vector2D Vector2D::zero(0.f, 0.f);
+Vector2D Vector2D::right(1.f, 0.f);
+Vector2D Vector2D::left(-1.f, 0.f);
+Vector2D Vector2D::up(0.f, 1.f);
+Vector2D Vector2D::down(0.f, -1.f);
+
+Vector2D operator+(Vector2D v1, Vector2D v2)
+{
+	return Vector2D::vectorAdd(v1, v2);
+}
+
+Vector2D operator-(Vector2D v1, Vector2D v2)
+{
+	return Vector2D::vectorSubtract(v1, v2);
+}
+
+Vector2D operator*(f32 scalar, Vector2D v)
+{
+	return Vector2D::scalarMultiply(scalar, v);
+}
+
+Vector2D operator*(Vector2D v, f32 scalar)
+{
+	return Vector2D::scalarMultiply(scalar, v);
+}
+
+Vector2D operator/(Vector2D v, f32 scalarInverse)
+{
+	return Vector2D::scalarMultiply(1.f / scalarInverse, v);
+}
+
+f32 operator*(Vector2D v1, Vector2D v2)
+{
+	return Vector2D::dotProduct(v1, v2);
+}
+
+bool operator==(Vector2D v1, Vector2D v2)
+{
+	return v1.x == v2.x
+		&& v1.y == v2.y;
+}
---
+++ CPPLWGE/CPPLWGE/Vector2D.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "Types.h"
+
+struct Vector2D
+{
+	f32 x;
+	f32 y;
+
+	Vector2D(f32 xx = 0.f, f32 yy = 0.f);
+
+	inline void normalize();
+	inline Vector2D unitVector();
+	inline f32 magnitude();
+	inline f32 magnitudeSquared();
+
+	inline f32 projectOntoMagnitude(Vector2D other);
+	inline Vector2D projectOnto(Vector2D other);
+	inline Vector2D orthogonalProjectionOnto(Vector2D other);
+	inline Vector2D reflectedAcross(Vector2D normal);
+
+	inline void operator+=(Vector2D v);
+	inline void operator-=(Vector2D v);
+	inline void operator*=(f32 scalar);
+	inline void operator/=(f32 scalar);
+
+	static inline Vector2D vectorAdd(Vector2D v1, Vector2D v2);
+	static inline Vector2D vectorSubtract(Vector2D v1, Vector2D v2);
+	static inline Vector2D scalarMultiply(f32 scalar, Vector2D v);
+	static inline f32 dotProduct(Vector2D v1, Vector2D v2);
+	static inline Vector2D scale(Vector2D v1, Vector2D v2);
+	static inline f32 crossProduct(Vector2D v1, Vector2D v2);
+	static inline f32 distance(Vector2D v1, Vector2D v2);
+	static inline f32 distanceSquared(Vector2D v1, Vector2D v2);
+	static inline f32 cosAngleBetween(Vector2D v1, Vector2D v2);
+	static inline f32 angleBetween(Vector2D v1, Vector2D v2);
+
+	static inline Vector2D lerp(Vector2D v1, Vector2D v2, f32 alpha);
+	static inline Vector2D slerp(Vector2D v1, Vector2D v2, f32 alpha);
+	static inline Vector2D moveTowards(Vector2D from, Vector2D to, f32 maxStep);
+	static inline Vector2D rotateTowards(Vector2D from, Vector2D to, f32 maxRotateStep, f32 maxMagnitudeStep);
+
+	static Vector2D zero;
+	static Vector2D right;
+	static Vector2D left;
+	static Vector2D up;
+	static Vector2D down;
+};
+
+inline Vector2D operator+(Vector2D v1, Vector2D v2);
+inline Vector2D operator-(Vector2D v1, Vector2D v2);
+inline Vector2D operator*(f32 scalar, Vector2D v);
+inline Vector2D operator*(Vector2D v, f32 scalar);
+inline Vector2D operator/(Vector2D v, f32 scalarInverse);
+inline f32 operator*(Vector2D v1, Vector2D v2);
+
+inline bool operator==(Vector2D v1, Vector2D v2);
