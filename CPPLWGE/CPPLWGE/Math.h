3b1198f58040af5ea40290e6c9566bcb3a12525c
Spencer Rawls <spencerrawls@tamu.edu>
2017-07-22 00:22:42 -0500

    Header and some unit tests for math namespace.

---
+++ CPPLWGE/CPPLWGE/Math.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "Types.h"
+
+namespace Math
+{
+	template <class T>
+	T min(T a, T b);
+
+	template <class T>
+	T max(T a, T b);
+
+	template <class T>
+	T clamp(T val, T min, T max);
+
+	f32 sin(f32 theta);
+	f32 cos(f32 theta);
+	f32 tan(f32 theta);
+	f32 arcsin(f32 ratio);
+	f32 arccos(f32 ratio);
+	f32 arctan(f32 ratio);
+	f32 arctan2(f32 y, f32 x);
+
+	f32 loge(f32 x);
+	f32 log2(f32 x);
+	f32 log10(f32 x);
+
+	f32 exp(f32 x);
+	f32 pow(f32 base, f32 exponent);
+	f32 pow(i32 base, f32 exponent);
+	f32 pow(f32 base, u32 exponent);
+	i32 pow(i32 base, u32 exponent);
+
+	f32 floor(f32 x);
+	f32 ceil(f32 x);
+	f32 roundTowardZero(f32 x);
+	f32 roundAwayFromZero(f32 x);
+	f32 round(f32 x);
+
+	f32 lerp(f32 a, f32 b, f32 t);
+	f32 inverseLerp(f32 a, f32 b, f32 result);
+
+	f32 smoothStep(f32 x);
+
+	template <class T>
+	T gcd(T a, T b);
+
+	template <class T>
+	T lcm(T a, T b);
+
+	template <class T>
+	bool isPrime(T n);
+
+	template <class T>
+	T factor(T n); // Just returns a single factor for now
+
+	template <class T>
+	T mod(T n, T modulus);
+
+	template <class T>
+	T inverseMod(T n, T modulus);
+
+	f32 sqrt(f32 x);
+	f32 cbrt(f32 x);
+
+	template <class T>
+	bool approximatelyEqual(T a, T b);
+
+
+	f32 PI;
+	f32 E;
+};
+
--- CPPLWGE/CPPLWGE/Tests.cpp
+++ CPPLWGE/CPPLWGE/Tests.cpp
@@ -3,8 +3,43 @@
 #include <iostream>
 
 // include other classes being tested here
+#include "Math.h"
 
 int main()
 {
 	// assert tests here
+	Assert(Math::min(1, 2) == 1, "Test failed for min(1, 2)");
+	Assert(Math::min(3, 2) == 2, "Test failed for min(3, 2)");
+	Assert(Math::min(1.2, 2.3) == 1.2, "Test failed for min(1.2, 2.3)");
+	Assert(Math::min(3.2f, 2.3f) == 2.3f, "Test failed for min(3.2, 2.3)");
+	
+	Assert(Math::max(1, 2) == 2, "Test failed for max(1, 2)");
+	Assert(Math::max(3, 2) == 3, "Test failed for max(3, 2)");
+	Assert(Math::max(1.2, 2.3) == 2.3, "Test failed for max(1.2, 2.3)");
+	Assert(Math::max(3.2f, 2.3f) == 3.2f, "Test failed for max(3.2, 2.3)");
+
+	Assert(Math::clamp(1, 0, 2) == 1, "Test failed for clamp(1, 0, 2)");
+	Assert(Math::clamp(-1, 0, 2) == 0, "Test failed for clamp(-1, 0, 2)");
+	Assert(Math::clamp(2, 0, 2) == 2, "Test failed for clamp(3, 0, 2)");
+	Assert(Math::clamp(0.f, 0.f, 1.f) == 0.f, "Test failed for clamp(0, 0, 1)");
+	Assert(Math::clamp(1.2f, 0.f, 1.f) == 1.f, "Test failed for clamp(1.2, 0, 1)");
+	Assert(Math::clamp(0.5f, 0.f, 1.f) == 0.5f, "Test failed for clamp(0.5, 0, 1)");
+
+	Assert(Math::approximatelyEqual(Math::sin(0.f), 0.f), "Test failed for sin(0)");
+	Assert(Math::approximatelyEqual(Math::sin(Math::PI / 6), 0.5f), "Test failed for sin(pi/6)");
+	Assert(Math::approximatelyEqual(Math::sin(Math::PI / 4), .7071f), "Test failed for sin(pi/4)");
+	Assert(Math::approximatelyEqual(Math::sin(Math::PI / 3), .866f), "Test failed for sin(pi/3)");
+	Assert(Math::approximatelyEqual(Math::sin(Math::PI / 2), 1.f), "Test failed for sin(pi/2)");
+
+	Assert(Math::approximatelyEqual(Math::cos(0.f), 1.f), "Test failed for cos(0)");
+	Assert(Math::approximatelyEqual(Math::cos(Math::PI / 6), .866f), "Test failed for cos(pi/6)");
+	Assert(Math::approximatelyEqual(Math::cos(Math::PI / 4), .7071f), "Test failed for cos(pi/4)");
+	Assert(Math::approximatelyEqual(Math::cos(Math::PI / 3), .5f), "Test failed for cos(pi/3)");
+	Assert(Math::approximatelyEqual(Math::cos(Math::PI / 2), 0.f), "Test failed for cos(pi/2)");
+
+
+	Assert(Math::approximatelyEqual(Math::tan(0.f), 0.f), "Test failed for tan(0)");
+	Assert(Math::approximatelyEqual(Math::tan(Math::PI / 6), .5774f), "Test failed for tan(pi/6)");
+	Assert(Math::approximatelyEqual(Math::tan(Math::PI / 4), 1.f), "Test failed for tan(pi/4)");
+	Assert(Math::approximatelyEqual(Math::tan(Math::PI / 3), 1.7321f), "Test failed for tan(pi/3)");
 }
